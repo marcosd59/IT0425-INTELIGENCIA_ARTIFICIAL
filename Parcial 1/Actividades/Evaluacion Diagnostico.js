73f1b18b07820c32ffbde6e63c504cda9c3bbbe3
marcosd59 <damian.marcospool@gmail.com>
2024-01-17 19:17:55 -0500

    Version Original

--- Parcial 1/Actividades/Evaluacion Diagnostico.js
+++ Parcial 1/Actividades/Evaluacion Diagnostico.js
@@ -1,4 +1,5 @@
-function validador(cadena) {
+function validador(cadena)
+{
   // Expresión regular que busca todas las letras (mayúsculas y minúsculas)
   let regExLetras = /[a-zA-Z]/g;
 
@@ -8,17 +9,14 @@ function validador(cadena) {
   // Expresión regular que busca todos los símbolos (cualquier carácter que no sea letra ni número)
   let reExSimbolos = /[^a-zA-Z0-9]/g;
 
-  // Utilizando las expresiones regulares para encontrar coincidencias en la cadena
+
   let letras = cadena.match(regExLetras);
   let numeros = cadena.match(regExNumeros);
   let simbolos = cadena.match(reExSimbolos);
 
-  // Devuelve un array con los resultados de las búsquedas
   return [letras, numeros, simbolos];
 }
 
-// Cadena de texto de ejemplo
-let cadena = "MarcosD59.net";
 
-// Llamada a la función validador con la cadena de texto y se imprime el resultado en la consola
+let cadena = "MarcosD59.net";
 console.log(validador(cadena));
